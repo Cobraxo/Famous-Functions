a81ecbcfcfacd25fa8aae47c6043597e3c94b5a0
Cobraxo <33578076+Cobraxo@users.noreply.github.com>
2019-09-06 18:09:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,22 +3,26 @@
 int main(void) {
 
     // Try my_putc
-    (void)my_putc('-', stdout);
+    (void)puts("\rTrying my_putc:");
+        (void)my_putc('+', stdout);
+    (void)putc('\n', stdout);
 
     // Try my_puts
-    (void)my_puts("How are you doing ?");
+    (void)puts("\rTrying my_puts:");
+        (void)my_puts("\tHow are you doing ?");
 
     // Try my_strncpy
-    char str[13];
-    my_strncpy(str, "Hello World !", 13);
-    printf("%s\n", str);
+    (void)puts("\rTrying my_strncpy");
+        char str[13];
+        my_strncpy(str, "Hello World !", 13);
+        printf("\t%s\n", str);
 
     // Try my_strchr
-    char *ptr = NULL, string[] = "Test sentence"; 
-    ptr = my_strchr(string, 's');   //We search 's' in "Test sentence"
-    printf("%c\n", *ptr);
+    (void)puts("\rTrying my_strchr");
+        char *ptr = NULL, string[] = "Test sentence"; 
+        ptr = my_strchr(string, 's');   //We search 's' in "Test sentence"
+        printf("\t%c\n", *ptr);
 
     return 0;
-
 
 }
